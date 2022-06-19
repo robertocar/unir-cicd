c7a44a5a061b49118cd03e96198a5c2817668411
robertocar <roberttosp@hotmail.com>
2022-06-19 14:45:11 -0500

    cicd

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -3,6 +3,20 @@ pipeline {
         label 'docker'
     }
     stages {
+        stage('PreBuild-Email') {
+           steps {
+               script {
+                   def mailRecipients = 'rcardenas@cntcloud.com'
+                   def jobName = currentBuild.fullDisplayName
+                   //emailext body: '''${SCRIPT, template="groovy-html.template"}''',
+                   emailext body: '''Hello''',
+                       mimeTye: 'text/html',
+                       subject: "[Jenkins] Started ${jobName}",
+                       to: "${mailRecipients}",
+                       replyTo: "${mailRecipients}",
+                       recipientProviders: [[$class: 'CulpritsRecipientProvider']]
+        }
+
         stage('Build') {
             steps {
                 echo 'Building stage!'
@@ -21,16 +35,30 @@ pipeline {
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
-        stage('Servicio tests') {
+        stage('EndtoEnd tests') {
             steps {
                 sh 'make test-e2e'
                 archiveArtifacts artifacts: 'results/*.xml'
             }
         }
+
+
     }
     post {
         always {
             junit 'results/*_result.xml'
+        }
+        
+        success {
+            emailext body: 'Check console output at $BUILD_URL to view the results. \n\n ${CHANGES} \n\n -------------------------------------------------- \n${BUILD_LOG, maxLines=100, escapeHtml=false}', 
+                    to: "${EMAIL_TO}", 
+                    subject: 'Build Success in Jenkins: $PROJECT_NAME - #$BUILD_NUMBER'
+        }
+        
+        failure {
+            emailext body: 'Check console output at $BUILD_URL to view the results. \n\n ${CHANGES} \n\n -------------------------------------------------- \n${BUILD_LOG, maxLines=100, escapeHtml=false}', 
+                    to: "${EMAIL_TO}", 
+                    subject: 'Build failed in Jenkins: $PROJECT_NAME - #$BUILD_NUMBER'
         }
     }
 }
