5e67aefef588f552433c956eb28f2442b844a4e7
robertocar <roberttosp@hotmail.com>
2022-06-19 21:26:34 -0500

    cicd

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -48,7 +48,7 @@ pipeline {
     post {
         always {
             junit 'results/*_result.xml'
-            
+            'subject: Build failed in Jenkins:  ${env.JOB_NAME}'
         }
         failure {
             /*emailext body: 'Check console output at $BUILD_URL to view the results. \n\n ${CHANGES} \n\n -------------------------------------------------- \n${BUILD_LOG, maxLines=100, escapeHtml=false}', 
@@ -56,7 +56,7 @@ pipeline {
                     subject: 'Build failed in Jenkins: $PROJECT_NAME - #$BUILD_NUMBER'*/
            /*echo "Check console output at $BUILD_URL to view the results. \n\n ${CHANGES} \n\n -------------------------------------------------- \n${BUILD_LOG, maxLines=100, escapeHtml=false}, 
                     to: "${EMAIL_TO}", */
-            echo   'subject: Build failed in Jenkins:  ${env.JOB_NAME}'
+            //echo   'subject: Build failed in Jenkins:  ${env.JOB_NAME}'
         
         }
     }
