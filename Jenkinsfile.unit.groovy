f5a9b835e7813f916156c2c812f7db8943d45c9b
robertocar <roberttosp@hotmail.com>
2022-06-19 21:44:12 -0500

    cicd

--- Jenkinsfile.unit.groovy
+++ Jenkinsfile.unit.groovy
@@ -57,7 +57,7 @@ pipeline {
                     subject: 'Build failed in Jenkins: $PROJECT_NAME - #$BUILD_NUMBER'*/
            /*echo "Check console output at $BUILD_URL to view the results. \n\n ${CHANGES} \n\n -------------------------------------------------- \n${BUILD_LOG, maxLines=100, escapeHtml=false}, 
                     to: "${EMAIL_TO}", */
-            echo   'subject: Build failed in Jenkins'
+            echo   'subject: Build failed in Jenkins '${env.JOB_NAME} [${env.BUILD_NUMBER}]''
         
         }
     }
